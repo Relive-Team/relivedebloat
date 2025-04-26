05dc53703ff7a1ace686c38b7c25709d83f66024
Matisio <mateuszmatisio@gmail.com>
2025-04-27 00:17:21 +0200

    Update firefox10.bat

--- Przegladarki/firefox10.bat
+++ Przegladarki/firefox10.bat
@@ -12,5 +12,5 @@ echo Instalator Firefoksa zostal pobrany i uruchomiony!
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Relive-Team/relivedebloat/refs/heads/main/10/debloat.bat' -OutFile '%temp%\debloat.exe'"
 
 :: Uruchamianie debloatu
-start "" "%temp%\debloat.exe"
+start "" "%temp%\debloat.bat"
 exit
