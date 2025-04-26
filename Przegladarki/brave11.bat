cc0bdce0bebd7f21ca44c74a383f53db20e8ec12
Matisio <mateuszmatisio@gmail.com>
2025-04-27 00:06:31 +0200

    Create brave11.bat

---
+++ Przegladarki/brave11.bat
@@ -0,0 +1,14 @@
+echo Wybrales Brave
+
+:: Pobieranie instalatora Brave
+powershell -Command "Invoke-WebRequest -Uri 'https://referrals.brave.com/latest/BraveBrowserSetup-BRV010.exe' -OutFile '%temp%\BraveInstaller.exe'"
+
+:: Uruchamianie instalatora
+start "" "%temp%\BraveInstaller.exe"
+
+:: Pobieranie debloatu
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Relive-Team/relivedebloat/refs/heads/main/11/debloat.bat' -OutFile '%temp%\debloat.exe'"
+
+:: Uruchamianie debloatu
+start "" "%temp%\debloat.exe"
+exit
