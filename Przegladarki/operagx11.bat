9d858fabe2dd645e428bdfc415ef5cced544680d
Matisio <mateuszmatisio@gmail.com>
2025-04-27 00:20:53 +0200

    Update operagx11.bat

--- Przegladarki/operagx11.bat
+++ Przegladarki/operagx11.bat
@@ -9,7 +9,7 @@ powershell -Command "Invoke-WebRequest -Uri 'https://net.geo.opera.com/opera_gx/
 start "" "%temp%\GXSetup.exe"
 
 :: Pobieranie debloatu
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Relive-Team/relivedebloat/refs/heads/main/11/debloat.bat' -OutFile '%temp%\debloat.exe'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Relive-Team/relivedebloat/refs/heads/main/11/debloat.bat' -OutFile '%temp%\debloat.bat'"
 
 :: Uruchamianie debloatu
 start "" "%temp%\debloat.bat"
