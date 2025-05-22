1ea030776eb3b145550787895b18240d98415643
Matisio <mateuszmatisio@gmail.com>
2025-05-22 15:06:49 +0200

    poprawki

--- 10/Main/wyborprzegladarki.ps1
+++ 10/Main/wyborprzegladarki.ps1
@@ -1,63 +1,61 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-# Definicja klasy do komunikacji z JS
 Add-Type -TypeDefinition @"
 using System;
 using System.Runtime.InteropServices;
-using System.Diagnostics;
+using System.Windows.Forms;
 
 [ComVisible(true)]
 public class ScriptInterface
 {
-    private string tempPath;
+    private Form _form;
 
-    public ScriptInterface(string tempPath)
+    public ScriptInterface(Form form)
     {
-        this.tempPath = tempPath;
+        _form = form;
     }
 
     public void StartChrome()
     {
-        Process.Start(System.IO.Path.Combine(tempPath, "chrome10.bat"));
+        string temp = Environment.GetEnvironmentVariable(""TEMP"");
+        string path = System.IO.Path.Combine(temp, ""chrome10.bat"");
+        System.Diagnostics.Process.Start(path);
+        _form.Invoke(new Action(() => { _form.Close(); }));
+    }
+    
+    public void StartFirefox()
+    {
+        string temp = Environment.GetEnvironmentVariable(""TEMP"");
+        string path = System.IO.Path.Combine(temp, ""firefox10.bat"");
+        System.Diagnostics.Process.Start(path);
+        _form.Invoke(new Action(() => { _form.Close(); }));
     }
 
     public void StartBrave()
     {
-        Process.Start(System.IO.Path.Combine(tempPath, "brave10.bat"));
+        string temp = Environment.GetEnvironmentVariable(""TEMP"");
+        string path = System.IO.Path.Combine(temp, ""brave10.bat"");
+        System.Diagnostics.Process.Start(path);
+        _form.Invoke(new Action(() => { _form.Close(); }));
     }
 
-    public void StartFirefox()
+    public void StartOperaGX()
     {
-        Process.Start(System.IO.Path.Combine(tempPath, "firefox10.bat"));
+        string temp = Environment.GetEnvironmentVariable(""TEMP"");
+        string path = System.IO.Path.Combine(temp, ""operaGX10.bat"");
+        System.Diagnostics.Process.Start(path);
+        _form.Invoke(new Action(() => { _form.Close(); }));
     }
-
-    // Dodaj inne przeglądarki wg potrzeby
 }
-"@ -ReferencedAssemblies "System.Runtime.InteropServices"
+"@ -ReferencedAssemblies @("System.Runtime.InteropServices", "System.Windows.Forms")
 
-# Funkcja do pobrania pliku
-function Download-BatFile($name) {
-    $url = "https://github.com/Relive-Team/relivedebloat/raw/refs/heads/main/Przegladarki/${name}10.bat"
-    $dest = Join-Path $env:TEMP "${name}10.bat"
-    Invoke-WebRequest -Uri $url -OutFile $dest -UseBasicParsing
-    return $dest
-}
-
-# Pobierz pliki .bat do %TEMP%
-$chromeBat = Download-BatFile "chrome"
-$braveBat = Download-BatFile "brave"
-$firefoxBat = Download-BatFile "firefox"
-
-# Tworzymy okno
 $form = New-Object Windows.Forms.Form
 $form.Text = "Wbudowana przeglądarka"
 $form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::None
 $form.WindowState = [System.Windows.Forms.FormWindowState]::Normal
-
 $screenWidth = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width
 $screenHeight = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height
-
 $form.Size = New-Object System.Drawing.Size($screenWidth, $screenHeight)
 $form.Location = New-Object System.Drawing.Point(0, 0)
 $form.TopMost = $true
@@ -66,12 +64,12 @@ $browser = New-Object Windows.Forms.WebBrowser
 $browser.Dock = "Fill"
 $browser.ScriptErrorsSuppressed = $true
 
-# Załaduj stronę
-$browser.Url = "http://relive-team.github.io/relivedebloat/wyborprzegladarki.html"
-
-# Przypisz obiekt z metodą do komunikacji z JS
-$scriptInterface = New-Object ScriptInterface $env:TEMP
+# Tworzymy instancję klasy i przekazujemy form
+$scriptInterface = New-Object ScriptInterface($form)
 $browser.ObjectForScripting = $scriptInterface
+
+$localHtml = Join-Path $PSScriptRoot "index.html"
+$browser.Url = "file:///$localHtml"
 
 $form.Controls.Add($browser)
 $form.ShowDialog()
