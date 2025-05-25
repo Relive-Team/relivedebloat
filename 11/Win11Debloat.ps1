75dfbd8d9fe63febe569a8caa6870f87e6269d3c
Matisio <mateuszmatisio@gmail.com>
2025-05-25 16:25:37 +0200

    poprawki

--- 11/Win11Debloat.ps1
+++ 11/Win11Debloat.ps1
@@ -1,5 +1,26 @@
 & ([scriptblock]::Create((irm "https://debloat.raphi.re/"))) -RunDefaults -Silent
+
 # Zamykanie procesów związanych z PowerShell i relivedebloat
+# Ścieżka do pliku VBS
+$vbscriptPath = "$env:TEMP\restart_system.vbs"
+
+# Treść skryptu VBS
+$vbscriptContent = @"
+Dim response
+response = MsgBox("Instalacja zostala zakonczona. Czy restartowac system? (zalecane)", vbYesNo + vbQuestion, "Restart systemu")
+
+If response = vbYes Then
+    Set objShell = CreateObject("WScript.Shell")
+    objShell.Run "shutdown.exe -r -t 0", 0, True
+End If
+"@
+
+# Zapisanie pliku VBS
+Set-Content -Path $vbscriptPath -Value $vbscriptContent -Encoding ASCII
+
+# Uruchomienie skryptu
+Start-Process "wscript.exe" -ArgumentList "`"$vbscriptPath`""
+
 Stop-Process -Name "powershell" -Force -ErrorAction SilentlyContinue
 Stop-Process -Name "pwsh" -Force -ErrorAction SilentlyContinue
 Stop-Process -Name "relivedebloat" -Force -ErrorAction SilentlyContinue
